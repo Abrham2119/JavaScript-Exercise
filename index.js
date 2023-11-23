f56f4923dd11f4ef042180d3cdcf4bb87bc347a8
Abrham2119 <abrhamzewdu2119@gmail.com>
2023-11-23 19:16:53 +0300

    i changed the format

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ let remainingCar = maxInputCar - inputUser.length;
 console.log("You have written " + inputUser.length + " characters, and you have " + remainingCar + " left.");
 
 
-
 // prompt and slice that count the charater the user used and the remaining
 
 let inputUser=prompt("Input your chatacter:");
